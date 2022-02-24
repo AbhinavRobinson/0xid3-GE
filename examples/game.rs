0cd708c74431c3675a860f8ee5cc8cfbd61de3d5
Abhinav Robinson <abhinavrobinson@gmail.com>
2022-02-24 22:07:36 +0530

    feat: doc

--- examples/game.rs
+++ examples/game.rs
@@ -1,3 +1,4 @@
 fn main() {
-    oxide_game_engine::clear_screen_to_color(0.0, 0.15, 0.25, 1.0);
+    // Make screen Nord Blue
+    oxide_game_engine::clear_screen_to_color(0.175, 0.2, 0.275, 1.0);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
+// Interfaces with JS code, inheritantly unsafe!
 extern "C" {
     pub fn js_clear_screen_to_color(r: f32, g: f32, b: f32, a: f32);
 }
 
+// Unsafe wrapper for js_clear_screen_to_color
 pub fn clear_screen_to_color(r: f32, g: f32, b: f32, a: f32) {
     unsafe {
         js_clear_screen_to_color(r, g, b, a);
