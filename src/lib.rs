ba2e045d77075fca4ef4c93ef0dacf940ccb2630
Abhinav Robinson <abhinavrobinson@gmail.com>
2022-02-27 21:54:44 +0530

    feat: add docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 extern "C" {
     /// Clears the screen to the given color.
     pub fn js_clear_screen_to_color(r: f32, g: f32, b: f32, a: f32);
+    /// draws a rectangle with given properties.
     pub fn js_draw_rectangle(x: f32, y: f32, width: f32, height: f32);
 }
 
@@ -31,6 +32,27 @@ pub fn clear_screen_to_color(r: f32, g: f32, b: f32, a: f32) {
     }
 }
 
+/// Draws a rectangle with given properties.
+///
+/// ### Arguments
+/// * `x` - X coordinate of the rectangle
+/// * `y` - Y coordinate of the rectangle
+/// * `width` - Width of the rectangle
+/// * `height` - Height of the rectangle
+///
+/// ### Example
+/// ```
+/// use oxide_game_engine::draw_rectangle;
+///
+/// draw_rectangle(0.0, 0.0, 100.0, 100.0);
+/// ```
+///
+/// ### Safety
+/// This function is unsafe because it calls a JS function that is not
+/// guaranteed to be safe.
+///
+/// ### Panics
+/// This function will panic if the JS function is not found.
 pub fn draw_rectangle(x: f32, y: f32, width: f32, height: f32) {
     unsafe {
         js_draw_rectangle(x, y, width, height);
@@ -43,6 +65,25 @@ thread_local! {
 }
 
 /// Add Event Handler to global
+///
+/// ### Arguments
+/// * `handler` - Event Handler
+///
+/// ### Example
+/// ```
+/// use oxide_game_engine::add_event_handler;
+///
+/// add_event_handler(|key| {
+///    println!("{:?}", key);
+/// });
+/// ```
+///
+/// ### Safety
+/// This function is unsafe because it calls a JS function that is not
+/// guaranteed to be safe.
+///
+/// ### Panics
+/// This function will panic if the JS function is not found.
 pub fn set_event_handler(function: impl FnMut(Key) + 'static) {
     EVENT_HANDLER.with(|event_handler| {
         *event_handler.borrow_mut() = Box::new(function);
@@ -71,6 +112,9 @@ extern "C" fn key_pressed(value: usize) {
 }
 
 /// Key press enums
+///
+/// ### Notes
+/// This enum is used to respond to key presses in javascript.
 pub enum Key {
     Left,
     Right,
